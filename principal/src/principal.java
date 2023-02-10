b08d581f56a4829d2f8674259615d7af26b7acc5
Estudiante USC <estudiante@GTACAD.USC>
2023-02-10 16:46:08 -0500

    actualizacion

--- principal/src/principal.java
+++ principal/src/principal.java
@@ -4,11 +4,18 @@ import java.util.Scanner;
 public class principal {
 	// metodo principál (main)
 	public static void main(String[] args) {
-		System.out.println("ingrese la palabra");
-//clase que permite capturar dator por consola
-		Scanner sc = new Scanner(System.in);
-		String prueba = sc.next();
-		System.out.println("la cantidad de letras que tiene la palabra es : " + prueba.length());
-
+		
+System.out.println(contarPalabras());
 	}
+	
+	
+	public static String contarPalabras() {
+	System.out.println("ingrese la palabra");
+	//clase que permite capturar dator por consola
+			Scanner sc = new Scanner(System.in);
+			String prueba = sc.next();
+			
+			return "la cantidad de letras que tiene la palabra es : ".concat(prueba.toLowerCase());
+
+}
 }
